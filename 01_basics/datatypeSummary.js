2d65e9763f482f0abc87442de7390eb9d10b73e1
Sahil Singh <74282295+traworld@users.noreply.github.com>
2024-01-25 20:35:25 +0000

    memory

--- 01_basics/datatypeSummary.js
+++ 01_basics/datatypeSummary.js
@@ -1,6 +1,6 @@
 // primitive and non primitive
 
-// in prmitive datatype we are given copy of the value from memory
+// in prmitive datatype we are given copy of the value from memory (stack memory)
 // 7 types : string , number , boolean , symbol , null , undefined
 
 const id = Symbol('233')
@@ -12,7 +12,7 @@ console.log(id == secId);  // false
 
 //symbol make the value different even if we provide the same value 
 
-// in non primitive we have give the exact reference of the value
+// in non primitive we have give the exact reference of the value (heap memory)
 // types : array , object , function
 // non primitive  datatype is function and function ka object function
 
