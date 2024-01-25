15dd824cd0edc36ec142dc864119873afe8596d4
Sahil Singh <74282295+traworld@users.noreply.github.com>
2024-01-25 18:45:48 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+// types of variables const , let , var
+// you cannot declare two variables the same id 
+
+const accountId = "1"
+// to declare a name or any word it should be in a  "" as a string 
+// in const the value can never be changed in  code
+
+let accountPassword = "1" 
+// we need to define let with ( " " ) or it wont be displayed
+
+
+console.table([accountId , accountPassword]);
+// two types of console .log and . table where table converts all the console id into table and print it
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // it wil be treated as newer versin of javascript cod e
+
+let name = "sammy"
+let nameId = 123
+let nameBranch;   // here it  means that the value is undefined coz we havent defined what kind of value gonna be stored
+let nameStream = null  // here the value is gonna be null that means the value is not 0 but null means empty or not available
+let nameCard = ""  // here we have declared the type that is string 
+
+console.log(typeof name);
+
+
+//types of primitive datatype
+/*
+number = maxing number assignes is 2 to the power 53 to the datatype number
+bigint = to store very large number that number datatype cant store
+string = " " In JavaScript, strings are sequences of characters enclosed in single (' '), double (" "), or backtick ( ) quotes. In JavaScript, a string is a sequence of characters, and these characters can include letters, numbers, symbols, spaces, and more
+boolean = true/flase
+undefined = above example 
+null
+symbol = to find uniqueness symbol is used
+
+
+
+object = null
+*/
