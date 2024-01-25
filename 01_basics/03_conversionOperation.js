db60c8abcbba99e9616470cbf880924c87775612
Sahil Singh <74282295+traworld@users.noreply.github.com>
2024-01-25 19:53:57 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -42,3 +42,42 @@ console.log(typeof valueInnumber5);
 // if we pass string then it will give output NaN
 
 //empty string in boolean will give false and value if given then true
+
+// ******************************** Operations *******************************
+
+let num = (2 + 3)
+let num1 = (2 - 3)
+let num2 = (2 * 3)
+let num3 = (2 ** 3) //it means 2 to the power 3
+let num4 = (2 / 3)
+let num5 = (2 % 3) // it gives remainder
+
+
+
+let num6 = ("2" + "3") //we can only add string not subtract
+console.log(num6);
+console.log(typeof num6);
+
+let num7 = (2 + "3") // it gives 23 as a output
+console.log(typeof num7);
+
+let num8 = ("2" + 3) // it gives output as 23
+console.log(typeof num8);
+
+let num9 = ("2" + "3" + 3) // it gives output as 233
+console.log(typeof num9);
+console.log(num9);
+
+let num10 = (2 + 3 + "5") // it gives output as 55
+console.log(num10);
+console.log(typeof num10);
+
+let num11 = (2 +"2"+ 3) // it gives remainder 223
+console.log(num11);
+
+// in operation if the first char is in string then js will treat other char as string too 
+// if the first char is number then it will treat other as a number 
+
+
+// there are other operators too called prefix and postfix
+// x++ ,  ++x
