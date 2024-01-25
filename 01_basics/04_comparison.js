55cc9b2961e4149534c547fab465cfdd97ce182f
Sahil Singh <74282295+traworld@users.noreply.github.com>
2024-01-25 20:06:09 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,19 @@
+console.log(3 != 3); 
+console.log(2 == 3);
+console.log(2 >= 3);
+console.log(2 <= 3);
+console.log(2 > 3);
+console.log(2 < 3);
+
+
+// eauality check == and comparisoons work differently in js 
+// here it converts null into 0 a number in comparison 
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+//in undefined every value is false if we campare
+
+// === strict check , it not only checks but strict check which means it checks datatype before conversion
+console.log("2" === 2);
