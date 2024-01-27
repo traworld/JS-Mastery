281db29d054f3b1a44e932d18aa453dc614d492e
Sahil Singh <74282295+traworld@users.noreply.github.com>
2024-01-27 21:56:01 +0000

    datesupdates

--- 01_basics/07_dates_in_JS.js
+++ 01_basics/07_dates_in_JS.js
@@ -14,3 +14,20 @@ console.log(date.toISOString());
 // to set a particular date we can write
 const newdate = new Date(2023,2,22)
 console.log(newdate.toDateString());
+
+let myTimeStamp = Date.now()
+
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
+console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+// `${newDate.getDay()} and the time `
+
+newDate.toLocaleString('default', {
+    weekday: "long",
+})
