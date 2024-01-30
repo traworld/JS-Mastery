c9661e6e3256caa21ee4940a45976fee1bfad57f
Sahil Singh <74282295+traworld@users.noreply.github.com>
2024-01-30 20:27:47 +0000

    arrow

--- 03_basics/03_arrow_func.js
+++ 03_basics/03_arrow_func.js
@@ -21,3 +21,44 @@ user.welcomemessage();  // we can directly run the function without moving it to
 
 // by printing only this method we get empty braces coz thir refers to something and if not is refered then it shows empty
 // but in browser it ahows window out  put because of the engines 
+// if we print this inside a function we get a lot of global values
+// ************** this function doesnot work in function if referenced but it can ork in object
+
+
+
+
+
+// syntax of arrow function ,, just remove function keyword from functin and write => beside ()
+
+const work = () => {
+    // this is a arrow function
+
+    let username = "sammy"
+    console.log(this.username);
+
+
+}
+
+work()    // output undefined coz this doesno work here
+
+//basic arrow function
+
+const value = (num1 , num2) => {           // in the  parametr we can add operators
+      return num1 + num2
+}
+
+console.log(value(3 , 4));
+
+// another arrow function  called implicit (maan leta hu aapko likhne ki zarorat nhi hai) return
+
+// in this we dont need to write {} and return in the  next line and just need to give the parameter of return on the same line
+
+
+const addTwo = (num , num1) => num+ num1   // we can write it as (num1 = num2)
+console.log(addTwo(3,6));
+
+// if we want to retur nan object we need to give parenthesis and inside  curly braces
+
+
+
+
