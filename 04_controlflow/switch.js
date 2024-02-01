88edf87e55d87879eab039dd4f958d94a3a25a84
Sahil Singh <74282295+traworld@users.noreply.github.com>
2024-02-01 11:36:22 +0000

    switch

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,27 @@
+// switch case statement
+
+// key is the value that i want to check everytime
+// in switch we can check multipe cases 
+// case value is the variable defined value
+// there is break if we meet our conditon and if therree is no break than it will print all the value below that without checking 
+
+
+const birthMonth = "april"
+switch (birthMonth) {
+    case "january":
+        console.log(`Not my birthMonth`);
+        break;
+    case "april":
+        console.log(`Hoorah!! its your BirthMonth`);
+        break;
+    case "december":
+        console.log(`not your BirthMonth`);
+        break;
+
+    default:console.log(`try other months`);
+        break;
+}
+
+// these conditions are based on true and false values
+
+
