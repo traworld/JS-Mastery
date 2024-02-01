49246f49e70e707c226e23a51a2766a4ae9837dd
Sahil Singh <74282295+traworld@users.noreply.github.com>
2024-02-01 15:18:33 +0000

    iterations

---
+++ 05_iterations/one.js
@@ -0,0 +1,16 @@
+// for loop jumps on various statement
+// syntax understanding in for loop first is variable initialize it define then var condition , then iterate it
+
+// for (let value = 0; value < array.length; value++) {
+//     const element = array[value];
+    
+// }
+
+for (let index = 0; index < 12; index++) {
+    const element = index;
+    if (element == 5) {
+        console.log(`here i 5 in the loop`);
+    }
+    console.log(element);
+    
+}
