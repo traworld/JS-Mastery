c21b95c510ba7f7b5d0895fd26a74eb9e054ef5a
Sahil Singh <74282295+traworld@users.noreply.github.com>
2024-02-17 07:25:15 +0000

    proj1

---
+++ 06_dom/domp1/script.js
@@ -0,0 +1,33 @@
+const btn = document.querySelectorAll(".button")
+const bdy = document.querySelector("body")
+
+// btn.forEach(function(btns){  // here for eahc btn( btn is all button as we stated in above variable)
+//    console.log(btn);          // if console is ritten inside the nwe have given a loop for a btn iside the function
+// })
+ 
+
+
+// this is done using loop 
+
+// btn.forEach(function(btns){
+//     //console.log(btns);
+
+//     btns.addEventListener('click', function (nameanything) {    // in event we have to carry a object which has all the evnts and we have to call that
+//         if (nameanything.target.id === "grey") {
+//             bdy.style.backgroundColor = nameanything.target.id
+//         }
+//         if (nameanything.target.id === "white") {
+//             bdy.style.backgroundColor = nameanything.target.id
+//         }
+//         if (nameanything.target.id === "blue") {
+//             bdy.style.backgroundColor = nameanything.target.id
+//         }
+//         if (nameanything.target.id === "yellow") {
+//             bdy.style.backgroundColor = nameanything.target.id
+//         }
+//     })
+// })
+// // event.target kis element se ye target aaya hai 
+
+// now using switch case
+
